771d5549d7ef73b5b5d8bf72bdfe793bb91cc120
Enzhe Gilyazova <gilyazovagilyazova@gmail.com>
2016-09-28 00:14:43 +0300

    HW

---
+++ HW/22.09/Task13.java
@@ -0,0 +1,31 @@
+class Task13 {
+
+	public static void main(String[] args){
+
+       
+         int r = Integer.parseInt(args[0]);
+
+        for (int x = 0; x <= 2 * r; x++){
+
+            for (int y = 0; y <= 2 * r; y++){
+
+                if ((x - r) * (x - r) + (y - r) * (y - r) < r * r) {
+
+                    System.out.print("**");
+
+                }
+
+                else {
+
+                    System.out.print("  ");
+
+                }
+
+            }
+
+            System.out.println();
+            
+        }
+    }
+
+}    
