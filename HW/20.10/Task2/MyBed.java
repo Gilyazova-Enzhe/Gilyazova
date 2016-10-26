bbdadd454827c49e3f3ab6deca94b4a3a4a462fc
Enzhe Gilyazova <gilyazovagilyazova@gmail.com>
2016-10-26 23:37:30 +0300

    HW

--- HW/20.10/Task2/MyBed.java
+++ HW/20.10/Task2/MyBed.java
@@ -1,7 +1,7 @@
 class MyBed {
 	public static void main(String[] args){
 		Bed bed1 = new Bed ("econom", 55, 100, true);
-		ModernBed bed2 = new ModernBed ("exclusive", 15,true, 15, true);
+		ModernBed bed2 = new ModernBed ("exclusive", 1,true, 15, true);
 	
 	System.out.println(bed1);
 	System.out.println(bed2);
