066767001aab416b964239e1e27e9994e414ed67
Enzhe Gilyazova <gilyazovagilyazova@gmail.com>
2016-11-02 21:55:23 +0300

    HW

--- HW/27.10/Rand.java
+++ HW/27.10/Rand.java
@@ -2,17 +2,17 @@ public class Rand {
 	public static void main (String[] args) {
 		int [] a = new int [10];
 		int b = 0;
-		System.out.println("The array:");
-		for (int i=0; i<a.length; i++){
-		a [i]= (int) (Math.random()*100);
-		System.out.print(a[i] + " ");
+			System.out.println("The array:");
+			for (int i=0; i<a.length; i++){
+				a [i]= (int) (Math.random()*100);
+				System.out.print(a[i] + " ");
 		}
 		
 		System.out.println ("\n" + "\n" + "reversed");
-		for (int i=0; i<a.length/2; i++){
-			b= a[i];
-			a[i]=a[a.length-i-1];
-			a[a.length-i-1]=b;
+			for (int i=0; i<a.length/2; i++){
+				b= a[i];
+				a[i]=a[a.length-i-1];
+				a[a.length-i-1]=b;
 		}
 		
 		for (int i=0; i<a.length; i++){
