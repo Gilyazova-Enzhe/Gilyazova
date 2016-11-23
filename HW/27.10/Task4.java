4268ebbc7300b8484c74bcab7fc5333fda788821
Enzhe Gilyazova <gilyazovagilyazova@gmail.com>
2016-11-23 23:35:52 +0300

    HW

---
+++ HW/27.10/Task4.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine(); //считывается с конс строка
+
+        System.out.println(isRightLine(line, 0, 0)); 
+    }
+
+    static boolean isRightLine(String line, int count, int i) { //i номер символа типо счетчика которые пробегает от начала до конца строки 
+        char tmp = line.charAt(i); //метод который возвращает символ по номеру
+        if (tmp == '(') {
+			count++; // прибавляет к счетчику 1
+		} else if (tmp == ')') {
+			count--;
+		}
+
+        if (count < 0) return false;
+        if (i != line.length() - 1) return isRightLine(line, count, i + 1);
+        return count == 0;
+    }
+}
