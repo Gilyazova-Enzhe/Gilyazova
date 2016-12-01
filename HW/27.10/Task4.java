71bf189fcba39761bf0e47bee20221f4b977b175
Enzhe Gilyazova <gilyazovagilyazova@gmail.com>
2016-12-01 14:30:41 +0300

    HW

--- HW/27.10/Task4.java
+++ HW/27.10/Task4.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine(); //считывается с конс строка
+        String line = sc.nextLine(); //считывается с конс строку
 
         System.out.println(isRightLine(line, 0, 0)); 
     }
 
-    static boolean isRightLine(String line, int count, int i) { //i номер символа типо счетчика которые пробегает от начала до конца строки 
+    static boolean isRightLine(String line, int count, int i) { //i номер символа (счетчик) который пробегает от начала до конца строки 
         char tmp = line.charAt(i); //метод который возвращает символ по номеру
         if (tmp == '(') {
 			count++; // прибавляет к счетчику 1
